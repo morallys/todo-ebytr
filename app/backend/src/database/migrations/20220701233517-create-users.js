ba6c34afc8de569461e9082f8d7ba48bb66765be
morallys <morallys.juan@gmail.com>
2022-07-04 14:48:35 -0300

    feat(database/migrations): Cria migração para Users

---
+++ app/backend/src/database/migrations/20220701233517-create-users.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
